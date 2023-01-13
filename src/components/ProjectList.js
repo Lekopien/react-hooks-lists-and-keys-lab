737ebad16be53110b13885ec4038c8e514b3bd3a
lekopien <mavericklekopien@gmail.com>
2023-01-14 00:04:16 +0300

    trial of links

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,7 +2,12 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+ // console.log(projects);
+ const projectElements = projects.map((project) => {
+  return (
+    <ProjectItem key={project.id} name={project.name} about={project.about} technologies={project.technologies} />
+    )
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
